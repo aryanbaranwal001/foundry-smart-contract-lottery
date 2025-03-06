26b958404d02e8d265cc6193c12876631e2a70b5
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-06 18:51:58 +0530

    added events and other basic funtionalities

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,12 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+/// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
 
+contract Raffle {
+    /* Errors */
+    error Raffle__NotEnoughEthSent();
 
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
 
+    /* Events */
+    event EnteredRaffle(address indexed player);
 
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
 
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) revert Raffle__NotEnoughEthSent();
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
 
+    function pickWinner() public {}
 
-
-
+    /**
+     * Getter Function
+     */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
