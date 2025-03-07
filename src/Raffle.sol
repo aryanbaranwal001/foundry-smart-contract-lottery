ddc68b8e30a9325a1d3e940f0827f5588ff3e0dc
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-07 21:52:24 +0530

    added checkUpkeep and performUpkeep

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -57,17 +57,31 @@ contract Raffle is VRFConsumerBaseV2Plus {
         s_raffelState = RaffleState.OPEN;
     }
 
+    // checks, effects, interactions patter
     function enterRaffle() external payable {
+        // checks, conditionals
         if (msg.value < i_entranceFee) revert Raffle__NotEnoughEthSent();
+        // effect (internal contract state changes)
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
+        // Interactions (external contract interactiosn)
         if (s_raffelState != RaffleState.OPEN) revert Raffle__RaffleNotOpen();
     }
 
-    /**
-     * Getter Function
-     */
-    function pickWinner() external returns (uint256) {
+    function checkUpkeep(bytes calldata /* checkData */ )
+        public
+        view
+        returns (bool upkeepNeeded, bytes memory /* performData */ )
+    {
+        bool timeHasPassed = (block.timestamp - s_lastTimeStamp >= i_interval);
+        bool isOpen = s_raffelState == RaffleState.OPEN;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+        upkeepNeeded = timeHasPassed && isOpen && hasBalance && hasPlayers;
+        return (upkeepNeeded, "");
+    }
+
+    function performUpkeep(bytes calldata /* performData */ ) external {
         // check to see if enough time has passed
         if (block.timestamp - s_lastTimeStamp < i_interval) revert();
         s_raffelState = RaffleState.CALCULATING;
@@ -81,17 +95,16 @@ contract Raffle is VRFConsumerBaseV2Plus {
             extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
         });
         uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
-        return (requestId);
     }
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
-        uint256 indexOfWinner = randomWords[0] % s_players[indexOfWinner];
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
         s_raffelState = RaffleState.OPEN;
-        s_players = new address payable[] (0);
-          s_lastTimeStamp = block.timestamp;
-          emit WinnerPicked(s_recentWinner);
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit WinnerPicked(s_recentWinner);
 
         (bool success,) = recentWinner.call{value: address(this).balance}("");
         if (!success) revert Raffle__TransferFailed();
