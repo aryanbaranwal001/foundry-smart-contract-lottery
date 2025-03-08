7d4c46aab0ff105db16b9afb884ade87e88cb447
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-08 22:52:31 +0530

    added vrfmock and handled some bugs

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import {Script} from "lib/forge-std/src/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
